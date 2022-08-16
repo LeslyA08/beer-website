cb130d1d6b7e656e35cc7ca8f43dc4b0bd483c4f
Lesly Alvarez <leslyalvarez08@gmail.com>
2022-08-16 15:25:07 -0500

    added code to fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
+
+  const [beers, setBeers] = useState ([])
+
+  //fetch api data
+  const getBeers = () => {
+    fetch ("https://punkapi.com/")
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      setUsers(data.results)
+    })
+  };
+
+
   return (
     <div>
       
